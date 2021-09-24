dcf5ef0460dd6c88f9a7f76b1de90ea281b88940
msPark <cjsan30@gmail.com>
2021-09-24 18:15:47 +0900

    input id convert to number and return userService.getUser

--- src/controller/user_controller.ts
+++ src/controller/user_controller.ts
@@ -1,4 +1,5 @@
-import { Controller } from "@nestjs/common"
+import { Controller, Get, Query, Req } from "@nestjs/common"
+import { User } from "../entity/user.entity"
 import { UserService } from "../service/user_service"
 
 
@@ -9,5 +10,14 @@ export class UserController {
 		private readonly userService: UserService
 	) { }
 
+	/**
+	 * Query string으로 id를 입력받고 Number화 하여 getUser로 반환
+	 * @param id 
+	 * @returns 
+	 */
+	@Get('/user')
+	public async getUser(@Query('id') id): Promise<User> {
+		return this.userService.getUser(Number(id))
+	}
 	
 }
