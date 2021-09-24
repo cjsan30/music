d0563c90b5f030b506f2f4516b6fbdfb3f9c22cf
msPark <cjsan30@gmail.com>
2021-09-24 18:39:20 +0900

    feat: add return user id method

--- src/repository/user_repository.ts
+++ src/repository/user_repository.ts
@@ -1,11 +1,12 @@
-import { AbstractRepository, EntityRepository } from "typeorm"
-import { User } from "../entity/user.entity"
+import { AbstractRepository, EntityRepository, MoreThan } from 'typeorm'
+import { User } from '../entity/user.entity'
+
 
 @EntityRepository(User)
 export class UserRepository extends AbstractRepository<User> {
 	
 	/**
-	 * id값을 입력받아 User 테이블에서 조회한 값을 반환.
+	 * receive parameter id and return id from song table
 	 * @param id 
 	 * @returns 
 	 */
@@ -13,27 +14,6 @@ export class UserRepository extends AbstractRepository<User> {
 		return await this.repository.findOne({
 			where: {
 				id: id
-			}
-		})
-	}
-
-	/**
-	 * User 타입의 user를 입력받아 테이블에 Insert
-	 * @param user 
-	 * @returns 
-	 */
-	public async insertUser(user: User): Promise<User> {
-		return await this.repository.save(user)
-	}
-
-	/**
-	 * Table에서 sex가 T인 user id를 반환
-	 * @returns 
-	 */
-	public async findUserIdMale(): Promise<User[]> {
-		return await this.repository.find({
-			where: {
-				sex: "T"
 			}
 		})
 	}
