ec1582c322ba67cc5a85b85a0eee30cc474f2ac6
msPark <cjsan30@gmail.com>
2021-09-24 17:18:59 +0900

    feat: add return user Id

--- src/repository/user_repository.ts
+++ src/repository/user_repository.ts
@@ -4,4 +4,16 @@ import { User } from "../entity/user.eneity";
 @EntityRepository(User)
 export class UserRepository extends AbstractRepository<User> {
 	
+	/**
+	 * id값을 입력받아 User 테이블에서 조회한 값을 반환.
+	 * @param id 
+	 * @returns 
+	 */
+	public async findUser(id: number): Promise<User> {
+		return await this.repository.findOne({
+			where: {
+				id: id
+			}
+		})
+	}
 }
